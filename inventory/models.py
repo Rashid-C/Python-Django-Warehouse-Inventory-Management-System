365029af1abe08773773f2a2627e0af63671e8f3
CodeCraft0001 <suhailthenoble@gmail.com>
2026-06-03 13:45:40 +0530

    Warehouse model

--- inventory/models.py
+++ inventory/models.py
@@ -9,3 +9,13 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Warehouse(models.Model):
+    company=models.ForeignKey(Company, on_delete=models.CASCADE,related_name='warehouses') #related_name pointing reverse relationship shortcut to parent(company)
+    name=models.CharField(max_length=255)
+    location_address=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.company.name})"
