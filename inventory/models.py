79fd80bfe37f8b1c340300069ef00d5fcc1f2c27
Rashid-C <anurashid11055@gmail.com>
2026-06-03 14:08:14 +0530

    Product model

--- inventory/models.py
+++ inventory/models.py
@@ -19,3 +19,15 @@ class Warehouse(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.company.name})"
+
+
+class Product(models.Model):
+    company=models.ForeignKey(Company,on_delete=models.CASCADE, related_name='products')
+    title=models.CharField(max_length=255)
+    sku=models.CharField(max_length=100,unique=True)           # Unique barcode/identifier
+    price=models.DecimalField(max_digits=10, decimal_places=2) # 399999999.99
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return f"{self.title} ({self.sku})"
