e04fd23b20282697dd8fbea9919e4aaf33ce7f51
CodeCraft0001 <suhailthenoble@gmail.com>
2026-06-03 13:30:48 +0530

    Company Model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name=models.CharField(max_length=255,unique=True)
+    address=models.TextField(blank=True, null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
