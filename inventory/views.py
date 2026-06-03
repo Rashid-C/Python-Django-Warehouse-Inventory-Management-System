3126517131598d3b12d98834ee82a666e12cce50
Rashid-C <anurashid11055@gmail.com>
2026-06-03 15:37:40 +0530

    ModelViewSet

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Company, Warehouse, Product, Stock
+from .serializers import CompanySerializer, WarehouseSerializer, ProductSerializer, StockSerializer
 
-# Create your views here.
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset=Company.objects.all()
+    serializer_class=CompanySerializer
+
+
+class WarehouseViewSet(viewsets.ModelViewSet):
+    queryset=Warehouse.objects.all()
+    serializer_class=WarehouseSerializer
+
+
+class ProductViewaSet(viewsets.ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+
+
+class StockViewSet(viewsets.ModelViewSet):
+    queryset=Stock.objects.all()
+    serializer_class=StockSerializer
