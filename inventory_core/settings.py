a91b46a2ac462eaa57776d17b020921af181c2d3
CodeCraft0001 <suhailthenoble@gmail.com>
2026-06-03 13:21:56 +0530

    log

--- inventory_core/settings.py
+++ inventory_core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Register your framework and custom app here:
+    'rest_framework',
+    'inventory',
 ]
 
 MIDDLEWARE = [
