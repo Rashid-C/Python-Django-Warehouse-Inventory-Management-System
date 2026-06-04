f7214bee2dac6d5a868c5ea128e21387e7cbd27d
Rashid-C <anurashid11055@gmail.com>
2026-06-04 12:04:16 +0530

    Added InventoryTaskViewSet

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,22 +1,18 @@
+# inventory/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CompanyViewSet, WarehouseViewSet, ProductViewSet, StockViewSet
-
-# Create a router
-router=DefaultRouter()
-
-# Create register
-router.register(r'companies',CompanyViewSet,basename='company')
-router.register(r'warehouse',WarehouseViewSet,basename='warehouse')
-router.register(r'products',ProductViewSet,basename='product')
-router.register(r'stocks',StockViewSet,basename='stock')
+from .views import CompanyViewSet, WarehouseViewSet, ProductViewSet, StockViewSet, InventoryTaskViewSet
 
 
+router = DefaultRouter()
 
-# The API URLs are now determined automatically by the router
+router.register(r'companies', CompanyViewSet, basename='company')
+router.register(r'warehouse', WarehouseViewSet, basename='warehouse')
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'stocks', StockViewSet, basename='stock')
+router.register(r'tasks', InventoryTaskViewSet, basename='task')
+
+
 urlpatterns = [
-    path('',include(router.urls))
+    path('', include(router.urls))
 ]
-
-
-
