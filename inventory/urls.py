93e049c13c0c832765e80a608fe29e0a34437473
Rashid-C <anurashid11055@gmail.com>
2026-06-04 17:36:13 +0530

    Register Payment Route

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,9 @@
 # inventory/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CompanyViewSet, WarehouseViewSet, ProductViewSet, StockViewSet, InventoryTaskViewSet,SaleViewSet,SaleItemViewSet,CustomerViewSet
+from .views import (CompanyViewSet, WarehouseViewSet, 
+    ProductViewSet, StockViewSet, InventoryTaskViewSet,
+SaleViewSet,SaleItemViewSet,CustomerViewSet ,PaymentViewSet)
 
 
 router = DefaultRouter()
@@ -14,6 +16,9 @@ router.register(r'tasks', InventoryTaskViewSet, basename='task')
 router.register(r'sales',SaleViewSet,basename='sale')
 router.register(r'sale-items',SaleItemViewSet,basename='saleitem')
 router.register(r'customers',CustomerViewSet)
+router.register(r'payments',PaymentViewSet)
+
+
 
 urlpatterns = [
     path('', include(router.urls))
