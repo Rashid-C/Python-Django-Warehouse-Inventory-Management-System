45faea658c8f0e854a3d73eb0dbf5a4d618693cd
Rashid-C <anurashid11055@gmail.com>
2026-06-04 17:31:33 +0530

    Payment Serializer

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
-from .models import Company,Warehouse,Product,Stock,InventoryTask,Sale,SaleItem,Customer
+from .models import (Company,
+Warehouse,
+Product,
+Stock,
+InventoryTask,
+Sale,SaleItem,
+Customer,Payment
+)
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -114,4 +121,9 @@ class SaleItemSerializer(serializers.ModelSerializer):
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
+        fields = '__all__'
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
         fields = '__all__'
